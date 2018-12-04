c7898b65b680015f9ecd5b3bb2defb204e27558d
Natascha <natascha.wade@btinternet.com>
2018-12-05 00:10:39 +0200

    add new todo

--- logic.js
+++ logic.js
@@ -13,25 +13,62 @@ class Header extends React.Component {
     render() {
         return (
             <div className="headerContainer">
-               <h2 className="header"> My Todo List</h2>
+                <h2 className="header"> My Todo List</h2>
             </div>
         )
     }
 }
 
 class List extends React.Component {
-    constructor(){
+    constructor() {
         super()
-       
+
+        this.newToDo = this.newToDo.bind(this),
+            this.remove = this.remove.bind(this),
+            this.showCompleted = this.showCompleted.bind(this),
+
+            this.state = {
+                arr: [],
+                key: 0,
+                removed: []
+            }
     }
-    newToDo() {
-        
+    //add new todo
+    newToDo(e) {
+        if (e.charCode == 13) {
+            this.setState({
+                arr: this.state.arr,
+                key: this.state.key + 1
+            })
+            this.state.arr.push(<li className="newTodo" key={this.state.key + 1}>{this.textInput.value} </li>)
+        }
     }
+    //remove todo when clicked on
+    remove(e) {
+        this.setState({
+            arr: this.state.arr,
+        })
+        this.state.removed.push(e.target)
+        this.state.arr.splice(e.target)
+    }
+    // figure out how to show removed items in this funtion
+    showCompleted() {
+        this.setState({
+            removed: this.state.removed,
+            // key: this.state.key + 1
+        })
+        this.state.removed.push(<li className="newTodo" key={this.state.key + 1}>{this.removed}</li>)
+    }
+
     render() {
         return (
-            <div className="inputContainer">
-                <img src="https://image.flaticon.com/icons/svg/54/54443.svg" /> 
-               <input onKeyPress={this.newToDo} className="addTodo" type="text" placeholder="Add a to-do..."/> 
+            <div className="inputContainer" >
+                <img src="https://image.flaticon.com/icons/svg/54/54443.svg" />
+                <input ref={(text) => { this.textInput = text; }} onKeyPress={this.newToDo} className="addTodo" type="text" placeholder="Add a to-do..." />
+                <span onClick={this.remove}>{this.state.arr}</span>
+
+                <div className="showCompleted"  onClick={this.showCompleted}>Show completed to-do's </div>
+                <div> {this.state.removed} </div>
             </div>
         )
     }
